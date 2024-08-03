88b653d24dbd9d7db5b85f665ba47fa6ca2bda4c
Arbyka <167877644+Arbyka@users.noreply.github.com>
2024-08-03 10:32:39 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Heading } from "@chakra-ui/react";
+
+import { sort, countDoubleNumber } from "./utils";
+
+export default function App() {
+  return (
+    <div>
+      <Heading>Hello Purwadhika</Heading>
+    </div>
+  );
+}
